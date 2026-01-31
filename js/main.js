3d626c32027af06e7e39a1d491415ea8151f0eef
mattriggle05 <mattriggle05@gmail.com>
2026-01-31 00:29:46 -0600

    Update wordlists, add testing code

--- js/main.js
+++ js/main.js
@@ -14,4 +14,18 @@ const categories = Object.keys(wordlists)
 let select = document.getElementById("solver-category-select");
 populateDropdown(select, categories)
 
+let out = {}
+for (let cat of categories) { 
+    for (let word of wordlists[cat]) { 
+        if (!word.match(/\s/g)) continue;
+            
+        let len = word.match(/\s/g).length + 1
 
+        if (len > 4) { 
+            if (!out[len]) out[len] = []
+
+            out[len].push(word)
+        }
+    }
+}
+console.log(out)

fdae60bdfb4b484c98c53fa9dfa3ba39a7448dd9
mattriggle05 <mattriggle05@gmail.com>
2026-01-31 04:38:45 -0600

    Expanding word search

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
 import { wordlists } from './wordlists.js'
 
 function populateDropdown(dropdownElement, optionsList) { 
-    let optionValue
-    for (optionValue of optionsList) {
+    for (let optionValue of optionsList) {
         var optionElement = document.createElement("option");
         optionElement.textContent = optionValue;
         optionElement.value = optionValue;
@@ -10,22 +9,36 @@ function populateDropdown(dropdownElement, optionsList) {
     }
 }
 
+function recalculatePossibleWords() {
+    var numSolutions = -1
+
+    var inputSelectedCategory = document.getElementById("solver-category-select").value
+    if (inputSelectedCategory == "null") return
+
+    var inputSelectedWordlist = wordlists[inputSelectedCategory]
+    numSolutions = inputSelectedWordlist.length
+    console.log("Category: " + numSolutions)
+}
+
+function repopulateNumWordsDropdown() { 
+    var inputSelectedCategory = document.getElementById("solver-category-select").value
+    if (inputSelectedCategory == "null") return
+
+    
+}
+
+
+for (let inputElement of document.getElementsByClassName("solver-input")) { 
+    inputElement.addEventListener('change', () => {
+        recalculatePossibleWords();
+    });
+}
+
+document.getElementsById("solver-category-select").addEventListener('change', () => {
+        recalculatePossibleWords();
+    });
+
 const categories = Object.keys(wordlists)
 let select = document.getElementById("solver-category-select");
 populateDropdown(select, categories)
-
-let out = {}
-for (let cat of categories) { 
-    for (let word of wordlists[cat]) { 
-        if (!word.match(/\s/g)) continue;
-            
-        let len = word.match(/\s/g).length + 1
-
-        if (len > 4) { 
-            if (!out[len]) out[len] = []
-
-            out[len].push(word)
-        }
-    }
-}
-console.log(out)
+recalculatePossibleWords()
